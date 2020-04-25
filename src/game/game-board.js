9d679213e05aa2153403746f8a49a13f86211e96
Chris Cain <ccain@nutshell.com>
2020-04-24 20:41:34 -0400

    Initial commit rendering a bingo board

---
+++ src/game/game-board.js
@@ -0,0 +1,78 @@
+import * as React from "react";
+import styles from "./game-board.module.css";
+
+export class GameBoard extends React.Component {
+	constructor() {
+		super();
+		this.state = {
+			bColumn: getNumbersInRange(1, 15),
+			iColumn: getNumbersInRange(16, 30),
+			nColumn: getNumbersInRange(31, 45),
+			gColumn: getNumbersInRange(46, 60),
+			oColumn: getNumbersInRange(61, 75),
+		};
+
+		console.log(this.state);
+	}
+
+	render() {
+		return (
+			<div className={styles.board}>
+				<Column topLetter="B" numbers={this.state.bColumn} />
+				<Column topLetter="I" numbers={this.state.iColumn} />
+				<Column topLetter="N" numbers={this.state.nColumn} />
+				<Column topLetter="G" numbers={this.state.gColumn} />
+				<Column topLetter="O" numbers={this.state.oColumn} />
+			</div>
+		);
+	}
+}
+
+class Column extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			selectedNumbers: getFiveRandomNumbersOrdered(props.numbers),
+		};
+	}
+	render() {
+		return (
+			<div className={styles.column}>
+				<div className={styles.header}>{this.props.topLetter}</div>
+				{this.state.selectedNumbers.map((number) => {
+					return (
+						<div className={styles.cell} key={number}>
+							{number}
+						</div>
+					);
+				})}
+			</div>
+		);
+	}
+}
+
+function getFiveRandomNumbersOrdered(range) {
+	const numbers = [];
+	while (numbers.length < 5) {
+		const newNumber = getRandomNumber(range[0], range[14]);
+
+		if (!numbers.includes(newNumber)) {
+			numbers.push(newNumber);
+		}
+	}
+
+	return numbers;
+}
+
+function getRandomNumber(min, max) {
+	return Math.floor(Math.random() * (max - min) + min);
+}
+
+function getNumbersInRange(min, max) {
+	const numbers = [];
+	for (let i = 0; i <= 14; i++) {
+		numbers.push(min + i);
+	}
+
+	return numbers;
+}
